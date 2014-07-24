3cf14942c7e9519944279b8b64f037c89b3ad7c0
Fabian Grünig <gruenig@posteo.de>
2014-07-24 12:42:50 +0200

    linker: get hostname correctly, ssh config

--- dotlinker.sh
+++ dotlinker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[ 0]}" )" && pwd )"
+THIS_HOST=`hostname`
+THIS_USER=`whoami`
 cd ${SCRIPTDIR}
 
 echo "Update Repository."
@@ -10,28 +12,29 @@ echo ""
 echo "Link dotfiles."
 for file in $(ls -l | awk 'NR!=1 && !/^d/ {print $NF}') ; 
 do
-  ln -vfs /home/${USER}/dotfiles/${file} /home/${USER}/.${file} 
+  ln -vfs /home/${THIS_USER}/dotfiles/${file} /home/${THIS_USER}/.${file} 
 done
 
 echo ""
 echo "Link host-specific dotfiles"
-ln -vfs /home/${USER}/dotfiles/i3/config.${HOST} /home/${USER}/dotfiles/i3/config
-unlink /home/${USER}/.xsession
-ln -vfs /home/${USER}/dotfiles/xsession.${HOST} /home/${USER}/.xsession
+echo "Hostname: ${THIS_HOST}"
+ln -vfs /home/${THIS_USER}/dotfiles/i3/config.${THIS_HOST} /home/${THIS_USER}/dotfiles/i3/config
+unlink /home/${THIS_USER}/.xsession
+ln -vfs /home/${THIS_USER}/dotfiles/xsession.${THIS_HOST} /home/${THIS_USER}/.xsession
 
 echo ""
 echo "Link dotfolders."
-ln -Tsfv /home/${USER}/dotfiles/i3/ /home/${USER}/.i3
-ln -Tsfv /home/${USER}/dotfiles/emacs.d/ /home/${USER}/.emacs.d
-ln -Tsfv /home/${USER}/dotfiles/elisp/ /home/${USER}/.elisp
-ln -Tsfv /home/${USER}/dotfiles/pentadactyl/ /home/${USER}/.pentadactyl
-ln -Tsfv /home/${USER}/dotfiles/zsh.d/ /home/${USER}/.zsh.d
-mv /home/${USER}/.ssh /home/${USER}/.ssh_bak
-unlink /home/${USER}/.ssh
-ln -Tsfv /home/${USER}/dotfiles/ssh/ /home/${USER}/.ssh
+ln -Tsfv /home/${THIS_USER}/dotfiles/i3/ /home/${THIS_USER}/.i3
+ln -Tsfv /home/${THIS_USER}/dotfiles/emacs.d/ /home/${THIS_USER}/.emacs.d
+ln -Tsfv /home/${THIS_USER}/dotfiles/elisp/ /home/${THIS_USER}/.elisp
+ln -Tsfv /home/${THIS_USER}/dotfiles/pentadactyl/ /home/${THIS_USER}/.pentadactyl
+ln -Tsfv /home/${THIS_USER}/dotfiles/zsh.d/ /home/${THIS_USER}/.zsh.d
+mv /home/${THIS_USER}/.ssh /home/${THIS_USER}/.ssh_bak
+unlink /home/${THIS_USER}/.ssh
+ln -Tsfv /home/${THIS_USER}/dotfiles/ssh/ /home/${THIS_USER}/.ssh
 
-rm /home/${USER}/.README
-rm /home/${USER}/.dotlinker.sh
+rm /home/${THIS_USER}/.README
+rm /home/${THIS_USER}/.dotlinker.sh
 
 echo ""
 echo "Done."
