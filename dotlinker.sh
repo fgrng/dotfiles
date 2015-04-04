df9edf5873cd1dc7f92aaa3e6dc5d3fd27f68e0a
Fabian Grünig <gruenig@posteo.de>
2015-04-04 11:42:11 +0200

    sh/zsh: Set PATH in profile. Profile host specific. More generic dotlinker script.

--- dotlinker.sh
+++ dotlinker.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[ 0]}" )" && pwd )"
 THIS_HOST=`hostname`
 THIS_USER=`whoami`
 cd ${SCRIPTDIR}
 
+echo ""
 echo "Update Repository."
 git pull
 
@@ -12,29 +13,35 @@ echo ""
 echo "Link dotfiles."
 for file in $(ls -l | awk 'NR!=1 && !/^d/ {print $NF}') ; 
 do
-  ln -vfs /home/${THIS_USER}/dotfiles/${file} /home/${THIS_USER}/.${file} 
+  ln -vfs ${SCRIPTDIR}/${file} ${HOME}/.${file} 
 done
 
 echo ""
 echo "Link host-specific dotfiles"
 echo "Hostname: ${THIS_HOST}"
-ln -vfs /home/${THIS_USER}/dotfiles/i3/config.${THIS_HOST} /home/${THIS_USER}/dotfiles/i3/config
-unlink /home/${THIS_USER}/.xsession
-ln -vfs /home/${THIS_USER}/dotfiles/xsession.${THIS_HOST} /home/${THIS_USER}/.xsession
+ln -vfs ${SCRIPTDIR}/i3/config.${THIS_HOST} ${SCRIPTDIR}/i3/config
+
+unlink ${HOME}/.xsession
+ln -vfs ${SCRIPTDIR}/xsession.${THIS_HOST} ${HOME}/.xsession
+
+unlink ${HOME}/.profile
+ln -vfs ${SCRIPTDIR}/profile.${THIS_HOST} ${HOME}/.profile
+ln -vfs ${SCRIPTDIR}/profile.${THIS_HOST} ${HOME}/.bash_profile
+ln -vfs ${SCRIPTDIR}/profile.${THIS_HOST} ${HOME}/.zprofile
 
 echo ""
 echo "Link dotfolders."
-ln -Tsfv /home/${THIS_USER}/dotfiles/i3/ /home/${THIS_USER}/.i3
-ln -Tsfv /home/${THIS_USER}/dotfiles/emacs.d/ /home/${THIS_USER}/.emacs.d
-ln -Tsfv /home/${THIS_USER}/dotfiles/elisp/ /home/${THIS_USER}/.elisp
-ln -Tsfv /home/${THIS_USER}/dotfiles/pentadactyl/ /home/${THIS_USER}/.pentadactyl
-ln -Tsfv /home/${THIS_USER}/dotfiles/zsh.d/ /home/${THIS_USER}/.zsh.d
-mv /home/${THIS_USER}/.ssh /home/${THIS_USER}/.ssh_bak
-unlink /home/${THIS_USER}/.ssh
-ln -Tsfv /home/${THIS_USER}/dotfiles/ssh/ /home/${THIS_USER}/.ssh
+ln -Tsfv ${SCRIPTDIR}/i3/ ${HOME}/.i3
+ln -Tsfv ${SCRIPTDIR}/emacs.d/ ${HOME}/.emacs.d
+ln -Tsfv ${SCRIPTDIR}/elisp/ ${HOME}/.elisp
+ln -Tsfv ${SCRIPTDIR}/pentadactyl/ ${HOME}/.pentadactyl
+ln -Tsfv ${SCRIPTDIR}/zsh.d/ ${HOME}/.zsh.d
+mv ${HOME}/.ssh ${HOME}/.ssh_bak
+unlink ${HOME}/.ssh
+ln -Tsfv ${SCRIPTDIR}/ssh/ ${HOME}/.ssh
 
-rm /home/${THIS_USER}/.README
-rm /home/${THIS_USER}/.dotlinker.sh
+rm ${HOME}/.README
+rm ${HOME}/.dotlinker.sh
 
 echo ""
 echo "Done."
