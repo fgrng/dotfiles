83efdee0f78c6a5d6d897753f761e9578c7939fd
Fabian Grünig <gruenig@posteo.de>
2018-06-15 21:52:45 +0200

    zsh: config emacs server env

--- zsh.d/plugins/emacs/emacs.plugin.zsh
+++ zsh.d/plugins/emacs/emacs.plugin.zsh
@@ -13,11 +13,12 @@
 # if "$ZSH/tools/require_tool.sh" emacs 24 2>/dev/null ; then
 if true ; then
     export EMACS_PLUGIN_LAUNCHER="$ZSH/plugins/emacs/emacsclient.sh"
+    export EMACSD_INSTANCE="instance1"
 
     # set EDITOR if not already defined.
     export EDITOR="${EDITOR:-${EMACS_PLUGIN_LAUNCHER}}"
     export ALTERNAT_EDITOR="nano"
-    
+
     # alias emacs="$EMACS_PLUGIN_LAUNCHER --no-wait"
     alias ems=emacs
 
@@ -28,29 +29,33 @@ if true ; then
     alias eeval="$EMACS_PLUGIN_LAUNCHER --eval"
 
     # create a new X frame
-    alias eframe='emacsclient --alternate-editor "" --create-frame & disown'
+    alias eframe='emacsclient --alternate-editor "" -nc -s ${EMACSD_INSTANCE} & disown'
     alias em=eframe
 
     # Write to standard output the path to the file
     # opened in the current buffer.
-    function efile {
+    function emfile {
         local cmd="(buffer-file-name (window-buffer))"
         "$EMACS_PLUGIN_LAUNCHER" --eval "$cmd" | tr -d \"
     }
 
     # Write to standard output the directory of the file
     # opened in the the current buffer
-    function ecd {
+    function emdir {
         local cmd="(let ((buf-name (buffer-file-name (window-buffer))))
                      (if buf-name (file-name-directory buf-name)))"
 
         local dir="$($EMACS_PLUGIN_LAUNCHER --eval $cmd | tr -d \")"
         if [ -n "$dir" ] ;then
-            echo "$dir"
+            return "$dir"
         else
             echo "can not deduce current buffer filename." >/dev/stderr
             return 1
         fi
+    }
+
+    function ecd {
+        cd $(emdir)
     }
 fi
 
